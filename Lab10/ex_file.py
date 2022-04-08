58e8e3f3f408a66c6bafde357e119c4fb77aaa45
mn-nattika <nattika.m@rmutvs.com>
2022-04-08 14:33:31 +0700

    test upload to github

---
+++ Lab10/ex_file.py
@@ -0,0 +1,16 @@
+# file I/O
+
+# open()
+#f = ""
+
+
+f = open ("demofile.txt",'a') # r - read
+print(f.read())
+f.write("\nThungsong")
+except Exception as e :
+
+    print(e)
+else:
+    print('File is already updated.')
+finally :
+    f.close()
